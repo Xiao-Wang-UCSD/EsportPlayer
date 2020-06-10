17dce3e293eac3489e3b125d16698fd8efd8d436
Xiao Wang <xiw304@ucsd.edu>
2020-06-10 00:05:42 -0700

    add data and line chart

--- app.py
+++ app.py
@@ -2,72 +2,28 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-@st.cache
-def get_data():
-    return pd.read_csv("http://data.insideairbnb.com/united-states/ny/new-york-city/2019-09-12/visualisations/listings.csv")
+st.title("If you want to be an esport player")
 
-df = get_data()
-st.title("Streamlit 101: An in-depth introduction")
-#st.markdown("Welcome to this in-depth introduction to [Streamlit](www.streamlit.io)! For this exercise, we'll use an Airbnb [dataset](http://data.insideairbnb.com/united-states/ny/new-york-city/2019-09-12/visualisations/listings.csv) containing NYC listings.")
+@st.cache
+def get_data(name):
+    if name = 'pie':
+        return pd.read_csv("https://raw.githubusercontent.com/Xiao-Wang-UCSD/EsportPlayer/master/data/pie-chart-data.csv")
+    if name = 'line':
+        return pd.read_csv("https://raw.githubusercontent.com/Xiao-Wang-UCSD/EsportPlayer/master/data/line-chart-data.csv")
+
+pie_df = get_data('pie')
 st.header("Pie Chart")
-values = st.selectbox("Year", ())
-selected_df = df['price'].where((df['price']>=values[0]) & (df['price']<=values[1]))
-f = px.histogram(selected_df, x="price", nbins=15, title="Price distribution")
-f.update_xaxes(title="Price")
-f.update_yaxes(title="No. of listings")
+years = tuple(pie_df['Year'].unique())
+values = st.selectbox("Year",years)
+selected_df = pie_df.where(pie_df['Year']==values)
+f = px.pie(selected_df, values='Players', names='Name', title='Player Population')
 st.plotly_chart(f)
 
-st.header("What is the distribution of availability in various neighborhoods?")
-st.write("Using a radio button restricts selection to only one option at a time.")
-st.write("💡 Notice how we use a static table below instead of a data frame. \
-Unlike a data frame, if content overflows out of the section margin, \
-a static table does not automatically hide it inside a scrollable area. \
-Instead, the overflowing content remains visible.")
-neighborhood = st.radio("Neighborhood", df.neighbourhood_group.unique())
-show_exp = st.checkbox("Include expensive listings")
-show_exp = " and price<200" if not show_exp else ""
+line_df = get_data()
+st.header("Line Chart")
+game = tuple(line_df['Name'].unique())
+values = st.selectbox("Game",game)
+selected_df = line_df.where(line_df['Year']==values)
+f = px.line(df, x="Year", y="Sales", title='Yearly Sales')
+st.plotly_chart(f)
 
-@st.cache
-def get_availability(show_exp, neighborhood):
-    return df.query(f"""neighbourhood_group==@neighborhood{show_exp}\
-        and availability_365>0""").availability_365.describe(\
-            percentiles=[.1, .25, .5, .75, .9, .99]).to_frame().T
-
-st.table(get_availability(show_exp, neighborhood))
-st.write("At 169 days, Brooklyn has the lowest average availability. At 226, Staten Island has the highest average availability.\
-    If we include expensive listings (price>=$200), the numbers are 171 and 230 respectively.")
-st.markdown("_**Note:** There are 18431 records with `availability_365` 0 (zero), which I've ignored._")
-
-df.query("availability_365>0").groupby("neighbourhood_group")\
-    .availability_365.mean().plot.bar(rot=0).set(title="Average availability by neighborhood group",
-        xlabel="Neighborhood group", ylabel="Avg. availability (in no. of days)")
-st.pyplot()
-
-st.header("Properties by number of reviews")
-st.write("Enter a range of numbers in the sidebar to view properties whose review count falls in that range.")
-minimum = st.sidebar.number_input("Minimum", min_value=0)
-maximum = st.sidebar.number_input("Maximum", min_value=0, value=5)
-if minimum > maximum:
-    st.error("Please enter a valid range")
-else:
-    df.query("@minimum<=number_of_reviews<=@maximum").sort_values("number_of_reviews", ascending=False)\
-        .head(50)[["name", "number_of_reviews", "neighbourhood", "host_name", "room_type", "price"]]
-
-st.write("486 is the highest number of reviews and two properties have it. Both are in the East Elmhurst \
-    neighborhood and are private rooms with prices $65 and $45. \
-    In general, listings with >400 reviews are priced below $100. \
-    A few are between $100 and $200, and only one is priced above $200.")
-st.header("Images")
-pics = {
-    "Cat": "https://cdn.pixabay.com/photo/2016/09/24/22/20/cat-1692702_960_720.jpg",
-    "Puppy": "https://cdn.pixabay.com/photo/2019/03/15/19/19/puppy-4057786_960_720.jpg",
-    "Sci-fi city": "https://storage.needpix.com/rsynced_images/science-fiction-2971848_1280.jpg"
-}
-pic = st.selectbox("Picture choices", list(pics.keys()), 0)
-st.image(pics[pic], use_column_width=True, caption=pics[pic])
-
-st.markdown("## Party time!")
-st.write("Yay! You're done with this tutorial of Streamlit. Click below to celebrate.")
-btn = st.button("Celebrate!")
-if btn:
-    st.balloons()
