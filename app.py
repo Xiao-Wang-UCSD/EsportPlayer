92e62167f8a96cdea5d7380c0c3f959ee6351d45
Xiao Wang <xiw304@ucsd.edu>
2020-06-10 21:43:50 -0700

    update survey

--- app.py
+++ app.py
@@ -164,21 +164,82 @@ f.update_traces(fill='toself')
 st.plotly_chart(f)
 
 
+# Survey section
+
+st.header("Survey")
 
 q4_str = "Do you like to participate in many small competitions or just a few big ones?"
-q4_selection = ['Many small ones','A good mixture','A few big ones']
+q4_selection = {'Many small ones':0,'A good mixture':1,'A few big ones':2}
 
 q2_str = "Do you want to compete as a pro player rather than professional player?"
-q2_selection = ["Yes, I don't have to be professional",'Not sure','Gotta be professional']
+q2_selection = {"Yes, I don't have to be professional":0,'Not sure':1,'Gotta be professional':2}
 
 q3_str = "Do you care about playing a trendy game?"
-q3_selection = ["Absolutely, if it's not trendy I won't play",'I dont care','No, I like classic games']
+q3_selection = {"Absolutely, if it's not trendy, I won't play":0,'I dont care':1}
 
 q1_str = "Are you very good at FPS game?"
-q1_selection = ['I am born for it','Very good','Not where my best is at']
+q1_selection = {'I am born for it':0,'Very good':1,'Not where my best is at':2}
 
-q1 = st.radio(q1_str,q1_selection)
-q2 = st.radio(q2_str,q2_selection)
-q3 = st.radio(q3_str,q3_selection)
-q4 = st.radio(q4_str,q4_selection)
+q1 = st.radio(q1_str,list(q1_selection))
+q2 = st.radio(q2_str,list(q2_selection))
+q3 = st.radio(q3_str,list(q3_selection))
+q4 = st.radio(q4_str,list(q4_selection))
+
+def calculate_score(q1_value,q2_value,q3_value,q4_value):
+
+    game_dic = {'Dota2':0,'CS:GO':0,'LOL':0,'Overwatch':0}
+
+    if q1_selection[q1_value]==0:
+        game_dic['CS:GO']+=2
+        #game_dic['LOL']+=1
+        game_dic['Overwatch']+=2
+    elif q1_selection[q1_value]==1:
+        game_dic['CS:GO']+=1
+        game_dic['LOL']+=1
+        game_dic['Overwatch']+=1
+        game_dic['Dota2']+=1
+    else:
+        game_dic['Dota2']+=2
+        game_dic['LOL']+=2
+
+    if q2_selection[q2_value]==0:
+        game_dic['CS:GO']+=2
+        game_dic['LOL']+=1
+        game_dic['Overwatch']+=1
+    elif q2_selection[q2_value]==1:
+        game_dic['LOL']+=1
+        game_dic['Overwatch']+=1
+        game_dic['CS:GO']+=1
+    else:
+        game_dic['Dota2']+=2
+
+    if q3_selection[q3_value]==0:
+        game_dic['LOL']+=0.5
+        game_dic['CS:GO']+=0.25
+        game_dic['Overwatch']-=0.5
+        game_dic['Dota2']-=0.5
+    elif q3_selection[q3_value]==1:
+        game_dic['LOL']+=1
+        game_dic['Overwatch']+=1
+        game_dic['CS:GO']+=1
+        game_dic['Dota2']+=1
+    if q4_selection[q4_value]==0:
+        game_dic['CS:GO']+=2
+        #game_dic['LOL']+=1
+        #game_dic['Overwatch']+=1
+    elif q4_selection[q4_value]==1:
+        game_dic['CS:GO']+=1
+        game_dic['LOL']+=2
+        game_dic['Overwatch']+=2
+        game_dic['Dota2']+=1
+    else:
+        game_dic['Dota2']+=2
+    return max(game_dic, key=game_dic.get)
+
+recommended = calculate_score(q1,q2,q3,q4)
+
+recommend_button = st.button('Recommend for me!')
+if recommend_button:
+    st.write(recommended)
+
 
