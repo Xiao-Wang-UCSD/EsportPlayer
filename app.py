8847b95366826a49b0536a4cd57d4051cfbc8562
Xiao Wang <xiw304@ucsd.edu>
2020-06-10 17:05:11 -0700

    center title

--- app.py
+++ app.py
@@ -19,18 +19,29 @@ def get_data(name):
     if name == 'full':
         return pd.read_csv("https://raw.githubusercontent.com/Xiao-Wang-UCSD/EsportPlayer/master/data/full-chart-data.csv")
 
+# Config
+title_config = {
+        'y':0.9,
+        'x':0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'}
+
+# Show the full dataframe
 full_df = get_data('full')
 st.header("Overview")
 st.write(full_df)
 
+# Show the market chart
 market_df = get_data('market')[['Revenue','Year']]
 st.header("Market Chart")
 selected_df = market_df
 selected_df = selected_df.dropna()
 f = px.bar(selected_df, x="Year", y = 'Revenue',title = 'Total Market Pool')
+f.update_layout(
+    title=title_config)
 st.plotly_chart(f)
 
-
+# Show the pie chart
 pie_df = get_data('pie')
 st.header("Pie Chart")
 years = tuple(pie_df['Year'].unique()) 
@@ -38,8 +49,11 @@ values = st.selectbox("Year",years)
 selected_df = pie_df.where(pie_df['Year']==values)
 selected_df = selected_df.dropna()
 f = px.pie(selected_df, values='Players', names='Name', title='Player Population')
+f.update_layout(
+    title=title_config)
 st.plotly_chart(f)
 
+# Show the line chart
 line_df = get_data('line')[['Name','Tournaments#','Year']]
 st.header("Line Chart")
 game = tuple(line_df['Name'].unique())
@@ -48,16 +62,22 @@ selected_df = line_df.where(line_df['Name']==values)
 selected_df = selected_df.dropna()
 
 f = px.line(selected_df, x="Year", y="Tournaments#",title='Yearly Tournament')
+f.update_layout(
+    title=title_config)
 st.plotly_chart(f)
 
+# Show the chart for total prize
 prize_df = get_data('prize')[['Name','Total Prize Poll#2016','Year']]
 st.header("Prize Chart")
 values = st.selectbox("Year ",years)
 selected_df = prize_df.where(prize_df['Year']==str(values))
 selected_df = selected_df.dropna()
 f = px.bar(selected_df, x="Name", y = 'Total Prize Poll#2016',title = 'Total Prize Pool')
+f.update_layout(
+    title=title_config)
 st.plotly_chart(f)
 
+# Show the radar chart
 radar_df = get_data('radar')
 st.header("Radar Chart")
 game = tuple(radar_df['Name'].unique())
@@ -69,6 +89,8 @@ selected_df.columns = ['feature','value']
 st.write(selected_df)
 
 f = px.line_polar(selected_df, r='value', theta='feature', line_close=True)
+f.update_layout(
+    title=title_config)
 f.update_traces(fill='toself')
 st.plotly_chart(f)
 
